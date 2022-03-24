b358f8906c819aa5fee5740ab396c786d21f6eac
narendraprasathnp7@gmail.com <narendraprasathnp7@gmail.com>
2022-03-24 11:29:59 +0530

    hello

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -5,7 +5,7 @@ import "./topbar.css";
 
 export default function TopBar() {
   const { user, dispatch } = useContext(Context);
-  const PF = "http://localhost:5000/images/"
+  const PF = "https://newbloggingapps.herokuapp.com/images/"
 
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
