d2c0d9d9afe3518645051588e550279863f141d3
narendraprasathnp7@gmail.com <narendraprasathnp7@gmail.com>
2022-03-24 17:17:07 +0530

    key

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("/api/posts" + search);
+      const res = await axios.get("/posts" + search);
       setPosts(res.data);
       
     };
